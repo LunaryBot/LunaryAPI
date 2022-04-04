3c072b39da064a5e72cfec006a74b3109388a72e
JVOPINHO <jvopinho.dev@gmail.com>
2022-04-03 23:21:13 -0300

    Rewrite Started

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var ws_1 = require("ws");
+var express_1 = __importDefault(require("express"));
+var http_1 = __importDefault(require("http"));
+require("dotenv/config");
+var app = (0, express_1["default"])();
+var server = http_1["default"].createServer(app);
+var wss = new ws_1.WebSocketServer({ server: server });
+app.use(express_1["default"].urlencoded({ extended: false }));
+app.use(require('cors')());
+server.listen(process.env.PORT, function () {
+    console.log("Server is running on port ".concat(process.env.PORT, " (http://localhost:").concat(process.env.PORT, ")"));
+});
--- src/index.ts
+++ src/index.ts
@@ -1,113 +1,16 @@
-import axios from 'axios';
-import express, { Request, Response } from 'express';
-import { Socket } from 'socket.io';
-import ActionManager from './structures/ActionManager';
-import { IGuildSuper, URLS, User, IGuildData } from './types';
-import { Permissions } from './Constants';
-import Databases from './structures/Databases';
-import GetGuildAction from './actions/getGuild';
+import { WebSocketServer } from 'ws';
+import express from 'express';
+import http from 'http';
 
-require('dotenv').config();
-
-const ps = Permissions.ADMINISTRATOR;
+import 'dotenv/config';
 
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
-});
-const dbs = new Databases();
-const manager = new ActionManager(dbs);
-const getGuild = new GetGuildAction();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(require('cors')());
 
-io.on('connection', async(socket: Socket) => {
-    const { token, guildId, fetchGuilds } = socket.handshake.query;
-
-    console.log(`Socket connected: ${socket.id}`);
-
-    if(!token) {
-        return disconnect('No token provided');
-    }
-    
-    const user = (await axios.get(URLS.USER, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }).catch(() => {}))?.data as User;
-
-    if(!user?.id) {
-        return disconnect('Invalid token');
-    }
-
-    const data: { user: User, guild?: IGuildSuper, guilds?: IGuildData[] } = { user };
-
-    if((guildId && typeof guildId == 'string') || fetchGuilds) {
-        const guilds = (await axios.get(URLS.GUILDS, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }).catch(() => {}))?.data as IGuildData[];
-
-        data.guilds = guilds;
-
-        if(guildId) {
-            const guild = guilds?.find(g => g.id == guildId);
-
-            if(!guild || (!guild.owner && !((guild.permissions & ps) == ps))) {
-                return disconnect('Missing Access');
-            };
-
-            const guildData = await getGuild.execute({
-                dbs,
-                guildId: guild.id,
-                user,
-                userId: user.id,
-                manager,
-            }, null);
-
-
-            data.guild = guildData;
-        };
-    }
-
-    socket.emit('ready', { data: { ...data } })
-
-    socket.onAny(async(event: string, data: any) => {
-        console.log(`Socket event: ${event}`);
-        if(event === 'disconnect') {
-            return;
-        };
-
-        const { op, ..._data } = await manager.execute({
-            op: event,
-            nonce: data?.nonce || null,
-            guildId: guildId?.toString() || null,
-            data: data?.data,
-            user,
-            userId: user.id
-        });
-
-        socket.emit(op, _data);
-    });
-
-    function disconnect(message: string) {
-        socket.emit('error', {
-            data: {
-                message
-            }
-        });
-        
-        socket.disconnect();
-        console.log(`Socket disconnected: ${socket.id}`);
-        return;
-    }
+server.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT} (http://localhost:${process.env.PORT})`);
 });
-
-
-server.listen(1);
