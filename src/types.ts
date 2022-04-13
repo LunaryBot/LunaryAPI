985cf7bb50a716f72ed590f94390335c29002983
JVOPINHO <jvopinho.dev@gmail.com>
2022-04-12 21:29:55 -0300

    Fix error

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,19 @@ export enum URLS {
 	USER = 'https://discord.com/api/users/@me',
 	GUILDS = 'https://discord.com/api/users/@me/guilds',
 }
+
+export interface User {
+	username: string;
+	id: string;
+	discriminator: string;
+	avatar: string|null;
+	public_flags: number;
+	banner?: string;
+	banner_color?: string;
+	accent_color?: number;
+	flags?: number;
+	locale?: string;
+	email?: string;
+	mfa_enabled?: boolean;
+	verified?: boolean;
+}
