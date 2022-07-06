a25be20f8e9c501febd28e576110e29fb7182374
João Pinho <jvopinho.dev@gmail.com>
2022-07-06 00:35:33 -0300

    Fixing bug of error message

--- src/utils/AuthChecker.ts
+++ src/utils/AuthChecker.ts
@@ -1,30 +1,40 @@
-import { ResolverData } from 'type-graphql';
+import { AuthChecker, ResolverData } from 'type-graphql';
 
 import GuildsResolver from '../resolvers/GuildsResolver';
 
 import { MyContext } from '../@types/Server';
 import ApiError from './ApiError';
 
-export default async function authChecker({ context }: ResolverData<MyContext>, permissions: Array<bigint>) {
-    const { userId, guildId } = context;
+const authChecker: AuthChecker<any, any> = async({ context }: ResolverData<MyContext>, permissions: Array<bigint>) => {
+    const { userId, guildId, token } = context;
+
+    if(token && !userId) throw new ApiError('Invalid/Expired token', 401);
 
     if(!userId) throw new ApiError('No token provided', 401);
 
-    if(permissions) {
-        if(permissions.length > 0) {
-            if(!guildId) return false;
+    const has = await (async() => {
+        if(permissions) {
+            if(permissions.length > 0) {
+                if(!guildId) return false;
+    
+                const member = await GuildsResolver.getMember(guildId, userId);
+                
+                if(!member) {
+                    return false;
+                }
+        
+                const permissionsResolved = permissions.reduce((a, b) => a | b, BigInt(0));
+        
+                return (BigInt(member.permissions) & permissionsResolved) === permissionsResolved;
+            } else return true;
+        }
+    
+        return true;
+    })();
 
-            const member = await GuildsResolver.getMember(guildId, userId);
-            
-            if(!member) {
-                return false;
-            }
-    
-            const permissionsResolved = permissions.reduce((a, b) => a | b, BigInt(0));
-    
-            return (BigInt(member.permissions) & permissionsResolved) === permissionsResolved;
-        } else return true;
-    }
+    if(!has) throw new ApiError('Missing Permissions', 403);
 
     return true;
 }
+
+export default authChecker;
