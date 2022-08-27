d355172f9c5c5fdbc4b05b5496445c5251dcf03d
jvopinho <jvopinho.dev@gmail.com>
2022-08-27 20:47:32 -0300

    Fixing

--- src/resolvers/GuildResolver.ts
+++ src/resolvers/GuildResolver.ts
@@ -1,13 +1,13 @@
 import { Resolver, Query, Ctx, Authorized, Arg } from 'type-graphql';
 
-import { Guild, User } from '@models';
+import { Guild } from '@models';
 
 import { MyContext } from '../@types/Server';
 
 @Resolver()
 class UserResolver {
     @Authorized()
-    @Query(() => User)
+    @Query(() => Guild)
 	async Guild(@Ctx() context: MyContext, @Arg('id') id: string) {
 		return await context.apollo.controllers.guilds.fetch(id);
 	}
