3e589216d9be3bdbc68dd45abe7fa67b0dcb2fa0
jvopinho <jvopinho.dev@gmail.com>
2022-09-05 11:25:22 -0300

    Adding PunishmentResolver

---
+++ src/resolvers/PunishmentResolver.ts
@@ -0,0 +1,25 @@
+import { PunishmentType } from '@prisma/client';
+import { Arg, Authorized, Ctx, Query, Resolver } from 'type-graphql';
+
+import { Punishment } from '@models';
+
+import { MyContext } from '../@types/Server';
+
+const nullable = { nullable: true };
+
+@Resolver()
+class PunishmentResolver {
+	@Query(() => [Punishment])
+	async Punishments(
+		@Ctx() context: MyContext,
+		@Arg('user_id', nullable) userId: string,
+        @Arg('author_id', nullable) authorId: string,
+        @Arg('type', () => String, nullable) type: PunishmentType,
+        @Arg('after', nullable) after: Date,
+        @Arg('before', nullable) before: Date
+	) {
+		return await context.apollo.controllers.punishments.fetch({ userId, authorId, type, after, before });
+	} 
+}
+
+export default PunishmentResolver;
