099fa7ced480690517835b83301d3239c7f234f8
jvopinho <jvopinho.dev@gmail.com>
2022-09-08 23:23:36 -0300

    Adding GuildFeatures Contants

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,7 +1,15 @@
 export const URLS = {
 	BASE: 'https://discord.com/api/oauth2/authorize',
 	TOKEN: 'https://discord.com/api/oauth2/token',
-	REVOKE:'https://discord.com/api/oauth2/token/revoke',
-	USER:'https://discord.com/api/users/@me',
-	GUILDS:'https://discord.com/api/users/@me/guilds',
+	REVOKE: 'https://discord.com/api/oauth2/token/revoke',
+	USER: 'https://discord.com/api/users/@me',
+	GUILDS: 'https://discord.com/api/users/@me/guilds',
+};
+
+export const GuildFeatures = {
+	mandatoryReasonToBan: 1n << 0n,
+	mandatoryReasonToKick: 1n << 1n,
+	mandatoryReasonToMute: 1n << 2n,
+	mandatoryReasonToAdv: 1n << 3n,
+	useHTMLTranscript: 1n << 4n,
 };
