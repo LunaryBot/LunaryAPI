4858c6011bba7cf8f9d42987843a308332726a96
jvopinho <jvopinho.dev@gmail.com>
2022-09-23 20:43:06 -0300

    Adding `GuildController.update`

--- src/controllers/GuildController.ts
+++ src/controllers/GuildController.ts
@@ -1,6 +1,6 @@
-import { Guild } from '@prisma/client';
+import { Guild, Prisma, PrismaPromise } from '@prisma/client';
 
-import { GuildGeneralSettingsValidation } from '@validation';
+import { GuildGeneralSettingsValidation, GuildPermissionsValidation } from '@validation';
 
 import GuildFeatures from '@utils/GuildFeatures';
 
@@ -18,20 +18,62 @@ class GuildController {
 		return await this.apollo.redis.get(`guilds:${guildId}`);
 	}
 
-	async update(guildId: string, { op, data }: { op: 'moderation', data: any }) {
-		const currentData = await this.apollo.prisma.guild.findUnique({
-			where: {
-				id: guildId,
-			},
-		}) || {} as Guild;
-
+	async update(guildId: string, { op, raw }: { op: 'moderation' | 'permissions', raw: any }) {
 		switch (op) {
 			case 'moderation': {
-				const raw = GuildGeneralSettingsValidation(data, currentData);
+				const currentData = await this.apollo.prisma.guild.findUnique({
+					where: {
+						id: guildId,
+					},
+				}) || {} as Guild;
 
-				console.log(raw, { ...raw, features: new GuildFeatures(raw.features as bigint || 0n).toArray(), id: guildId });
+				const data = GuildGeneralSettingsValidation(raw, currentData);
 
-				return { ...raw, features: new GuildFeatures(raw.features as bigint || 0n).toArray(), id: guildId };
+				return { ...data, features: new GuildFeatures(data.features as bigint || 0n).toArray(), id: guildId };
+			}
+
+			case 'permissions': {
+				const data = GuildPermissionsValidation(raw, guildId);
+
+				const ids = data.map(({ id }) => id);
+
+				const currentData = await this.apollo.prisma.guildPermissions.findMany({
+					where: {
+						guild_id: guildId,
+					},
+				});
+
+				const deleteIds = currentData.filter(({ id }) => !ids.includes(id)).map(({ id }) => id);
+
+				const args: PrismaPromise<any>[] = [];
+
+				if(deleteIds.length) {
+					args.push(this.apollo.prisma.guildPermissions.deleteMany({
+						where: {
+							id: {
+								in: deleteIds,
+							},
+							guild_id: guildId,
+						},
+					}));
+				}
+
+				data.forEach(permissions => {
+					args.push(this.apollo.prisma.guildPermissions.upsert({
+						where: {
+							guild_id_id: {
+								guild_id: guildId,
+								id: permissions.id,
+							},
+						},
+						create: { ...permissions, guild_id: guildId },
+						update: permissions,
+					}));
+				});
+
+				this.apollo.prisma.$transaction(args);
+
+				return JSON.parse(JSON.stringify(data, (k, v) => typeof v == 'bigint' ? Number(v) : v));
 			}
 		}
 	}
