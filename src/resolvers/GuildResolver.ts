5ddcd3fc7768402997f3b9871fb2b59c81563135
jvopinho <jvopinho.dev@gmail.com>
2022-09-23 20:43:46 -0300

    Adding GuildResolver methods to update guild data

--- src/resolvers/GuildResolver.ts
+++ src/resolvers/GuildResolver.ts
@@ -1,6 +1,6 @@
 import { Resolver, Query, Ctx, Authorized, Arg, Mutation } from 'type-graphql';
 
-import { Guild, GuildSettings, GuildSettingsInput } from '@models';
+import { Guild, GuildPermissions, GuildPermissionsInput, GuildSettings, GuildSettingsInput } from '@models';
 
 import { MyContext } from '../@types';
 
@@ -14,9 +14,14 @@ class GuildResolver {
 
 	// @Authorized()
 	@Mutation(type => GuildSettings)
-    async ModifyGuildModerationSettings(@Ctx() context: MyContext, @Arg('id') id: string, @Arg('data') data: GuildSettingsInput) {
-    	return await context.apollo.controllers.guilds.update(id, { op: 'moderation', data });
+    async ModifyGuildModerationSettings(@Ctx() context: MyContext, @Arg('id') id: string, @Arg('raw') raw: GuildSettingsInput) {
+    	return await context.apollo.controllers.guilds.update(id, { op: 'moderation', raw });
     }
+
+	@Mutation(type => [GuildPermissions])
+	async ModifyGuildPermissions(@Ctx() context: MyContext, @Arg('id') id: string, @Arg('raw', type => [GuildPermissionsInput]) raw: GuildPermissionsInput[]) {
+    	return await context.apollo.controllers.guilds.update(id, { op: 'permissions', raw });
+	}
 }
 
 export default GuildResolver;
