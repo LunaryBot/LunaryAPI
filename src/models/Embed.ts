8c48b719005c2d6d71cd7c4aa270d16ec9b7f7e3
jvopinho <jvopinho.dev@gmail.com>
2022-09-23 23:08:53 -0300

    Adding `@models/Embed`

---
+++ src/models/Embed.ts
@@ -0,0 +1,47 @@
+import { ObjectType, Field } from 'type-graphql';
+
+import { 
+	EmbedAuthorBase, 
+	EmbedBase, 
+	EmbedFieldBase, 
+	EmbedFooterBase, 
+	EmbedImageBase, 
+	EmbedThumbnailBase, 
+} from './utils/embed';
+
+const nullable = { nullable: true };
+
+@ObjectType()
+class EmbedAuthor extends EmbedAuthorBase {}
+
+@ObjectType()
+class EmbedField extends EmbedFieldBase {}
+
+@ObjectType()
+class EmbedFooter extends EmbedFooterBase {}
+
+@ObjectType()
+class EmbedImage extends EmbedImageBase {}
+
+@ObjectType()
+class EmbedThumbnail extends EmbedThumbnailBase {}
+
+@ObjectType()
+class Embed extends EmbedBase {
+    @Field(type => EmbedFooter, nullable)
+    	footer?: EmbedFooter;
+
+    @Field(type => EmbedImage, nullable)
+    	image?: EmbedImage;
+
+    @Field(type => EmbedThumbnail, nullable)
+    	thumbnail?: EmbedThumbnail;
+
+    @Field(type => EmbedAuthor, nullable)
+    	author?: EmbedAuthor;
+
+    @Field(type => [EmbedField], nullable)
+    	fields?: EmbedField[];
+}
+
+export { Embed };
