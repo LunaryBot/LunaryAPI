4ba91d189e8b56f5efb77cd8f67c0a77c83aaee0
jvopinho <jvopinho.dev@gmail.com>
2022-09-29 00:06:12 -0300

    Adding Embed schema

---
+++ src/utils/schemas/Embed.ts
@@ -0,0 +1,43 @@
+import Schema, { SchemaOption, SchemaType, SchemaTypeBase } from '../Schema';
+
+const imageBase = {
+	url: make(String),
+	proxy_url: make(String),
+	height: make(Number),
+	width: make(Number),
+};
+
+const EmbedSchema = new Schema({
+	content: make(String),
+	embed: make({
+		title: make(String),
+		description: make(String),
+		url: make(String),
+		timestamp: make(Boolean),
+		color: make(Number),
+		footer: {
+			text: make(String),
+			icon_url: make(String),
+			proxy_icon_url: make(String),
+		},
+		image: imageBase,
+		thumbnail: imageBase,
+		author: {
+			name: make(String),
+			url: make(String),
+			icon_url: make(String),
+			proxy_icon_url: make(String),
+		},
+		fields: make([{
+			name: make(String, false),
+			value: make(String, false),
+			inline: make(Boolean),
+		}]),
+	}),
+});
+
+export { EmbedSchema };
+
+function make(_type: SchemaTypeBase, nullable = true): SchemaOption {
+	return { _type, nullable };
+}
