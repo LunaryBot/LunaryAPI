cd8bc179e96456ff113471cd1aa842f4be9dcff7
jvopinho <jvopinho.dev@gmail.com>
2022-10-08 22:38:17 -0300

    Adding mutations `DeleteGuildEmbed` & `ModifyGuildEmbed`

--- src/resolvers/GuildResolver.ts
+++ src/resolvers/GuildResolver.ts
@@ -1,35 +1,71 @@
 import { EmbedType } from '@prisma/client';
 import { Resolver, Query, Ctx, Authorized, Arg, Mutation } from 'type-graphql';
 
-import { Embed, Guild, GuildPermissions, GuildPermissionsInput, GuildSettings, GuildSettingsInput } from '@models';
+import ApiError from '@utils/ApiError';
 
 import { MyContext } from '../@types';
+import { EmbedInput, GuildPermissionsInput, GuildSettingsInput } from '@inputs';
+import { Embed, Guild, GuildPermissions, GuildSettings } from '@models';
 
 @Resolver()
 class GuildResolver {
     @Authorized()
     @Query(type => Guild)
-	async Guild(@Ctx() context: MyContext, @Arg('id') id: string) {
+	async Guild(
+		@Ctx() context: MyContext, 
+		@Arg('id') id: string
+	) {
 		return await context.apollo.controllers.guilds.fetch(id);
 	}
 
-	@Query(type => Embed)
-    async GuildEmbeds(@Ctx() context: MyContext, @Arg('id') id: string) {
-    	return {
-    		type: EmbedType.BAN,
-    		guild_id: id,
-    		content: 'a',
-    	};
+	@Query(type => [Embed], { defaultValue: [] })
+    async GuildEmbeds(
+		@Ctx() context: MyContext, 
+		@Arg('id') id: string, 
+		@Arg('type', { nullable: true }) type?: EmbedType
+    ) {
+    	if(type && !EmbedType[type]) {
+    		throw new ApiError('Invalid type', 400);
+    	}
+		
+    	return await context.apollo.controllers.guilds.fetchEmbed(id, type);
     }
 
 	// @Authorized()
+	@Mutation(type => Boolean)
+	async DeleteGuildEmbed(
+		@Ctx() context: MyContext, 
+		@Arg('id') id: string, 
+		@Arg('type') type: EmbedType
+	) {
+		return await context.apollo.controllers.guilds.update(id, { op: 'embeds', raw: { type } });
+	}
+
+	@Mutation(type => Embed)
+	async ModifyGuildEmbed(
+		@Ctx() context: MyContext, 
+		@Arg('id') id: string, 
+		@Arg('type') type: EmbedType, 
+		@Arg('data', type => EmbedInput) raw: EmbedInput
+	) {
+		return await context.apollo.controllers.guilds.update(id, { op: 'embeds', raw: { ...raw, type } });
+	}
+
 	@Mutation(type => GuildSettings)
-	async ModifyGuildModerationSettings(@Ctx() context: MyContext, @Arg('id') id: string, @Arg('raw') raw: GuildSettingsInput) {
+	async ModifyGuildModerationSettings(
+		@Ctx() context: MyContext, 
+		@Arg('id') id: string, 
+		@Arg('data') raw: GuildSettingsInput
+	) {
     	return await context.apollo.controllers.guilds.update(id, { op: 'moderation', raw });
 	}
 
 	@Mutation(type => [GuildPermissions])
-	async ModifyGuildPermissions(@Ctx() context: MyContext, @Arg('id') id: string, @Arg('raw', type => [GuildPermissionsInput]) raw: GuildPermissionsInput[]) {
+	async ModifyGuildPermissions(
+		@Ctx() context: MyContext, 
+		@Arg('id') id: string, 
+		@Arg('data', type => [GuildPermissionsInput]) raw: GuildPermissionsInput[]
+	) {
     	return await context.apollo.controllers.guilds.update(id, { op: 'permissions', raw });
 	}
 }
