e27fb382175890ed833f95c77f972028f56d9d75
jvopinho <jvopinho.dev@gmail.com>
2022-10-21 17:35:08 -0300

    Adding GuildReasonsValidation

---
+++ src/utils/validation/guild/GuildReasonsValidation.ts
@@ -0,0 +1,45 @@
+import { PunishmentType, Reason } from '@prisma/client';
+
+import ApiError from '@utils/ApiError';
+import Schema from '@utils/Schema';
+import { ReasonSchema } from '@utils/schemas';
+
+const _types = Object.keys(PunishmentType);
+
+const ReasonArray = new Schema([ReasonSchema] as any);
+
+const maxMuteDuration = 28 * 1000 * 60 * 60 * 24;
+const minMuteDuration = 1 * 1000;
+
+const days = [0, 1, 7];
+
+function GuildReasonsValidation(data: (Omit<Reason, 'guild_id' | 'id'> & { id?: string })[]) {
+	const errors: string[] = [];
+
+	const reasons = data.map((reason, i) => {
+		const types = [ ...new Set(reason.types.filter(_types.includes.bind(_types))) ];
+
+		if(types.length <= 0) {
+			errors.push(`raw[${i}].types: this field is required`);
+		}
+
+		if(typeof reason.duration == 'number' && (reason.duration < minMuteDuration || reason.duration > maxMuteDuration)) {
+			errors.push(`raw[${i}].duration: invalid duration`);
+		}
+
+		if(typeof reason.days == 'number' && !days.includes(reason.days)) {
+			errors.push(`raw[${i}].days: invalid days`);
+		}
+		
+		return ({
+			...reason,
+			types,
+		});
+	});
+
+	if(errors.length) throw new ApiError('Invalid Raw', 400, errors);
+
+	return ReasonArray.parse(reasons);
+}
+
+export { GuildReasonsValidation };
