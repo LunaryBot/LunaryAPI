8c247ccb20b9480d8439dff6c1251201b32c8a20
jvopinho <jvopinho.dev@gmail.com>
2022-12-04 21:28:12 -0300

    Adding `UserFeatures`

---
+++ src/structures/Database/User/UserFeatures.ts
@@ -0,0 +1,10 @@
+import BitField from '../../../utils/BitField';
+import * as Constants from '../../../utils/Constants';
+
+type UserFeature = keyof typeof Constants.UserFeatures;
+
+class UserFeatures extends BitField<UserFeature> {
+	public static Flags = Constants.UserFeatures;
+}
+
+export { UserFeatures };
