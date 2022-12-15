16ace7abc2efdec7d59306ed49467742aa7ab885
jvopinho <jvopinho.dev@gmail.com>
2022-12-14 22:43:51 -0300

    Adding UserDatabase model

---
+++ src/@types/models/user/UserDatabase.ts
@@ -0,0 +1,37 @@
+import { User, UserPremiumType } from '@prisma/client';
+import { Field, ID, ObjectType, UseMiddleware } from 'type-graphql';
+
+import DefaultValue from '@utils/DefaultValue';
+
+const nullable = { nullable: true };
+
+@ObjectType()
+class UserDatabase implements Omit<User, 'id' | 'features' | 'inventory' | 'inventory_using' | 'flags'> {
+    @UseMiddleware(DefaultValue<string[]>([]))
+    @Field(type => [String])
+    	features: string[];
+
+    @Field(type => Number, nullable)
+    	luas: number | null;
+
+    @Field(type => Date, nullable)
+    	last_daily_at: Date | null;
+
+    @Field(type => String, nullable)
+    	bio: string | null;
+
+    @UseMiddleware(DefaultValue<number>(0))
+    @Field(type => Number)
+    	xp: number;
+
+    @Field(type => String, nullable)
+    	premium_type: UserPremiumType | null;
+
+    @Field(type => Date, nullable)
+    	premium_until: Date | null;
+
+    @Field(type => [String])
+    	flags: bigint | null;
+}
+
+export { UserDatabase };
