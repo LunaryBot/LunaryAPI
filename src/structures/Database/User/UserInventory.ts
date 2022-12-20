b873a96a963f13d200aa9b1de170cf96c708edcc
jvopinho <jvopinho.dev@gmail.com>
2022-12-20 19:33:12 -0300

    Adding UserInventory in `@Database/User`

---
+++ src/structures/Database/User/UserInventory.ts
@@ -0,0 +1,12 @@
+import BitField from '@utils/BitField';
+import { InventoryItems } from '@utils/contants/InventoryItems';
+
+class UserInventory extends BitField {
+	public static Flags = Object.fromEntries(InventoryItems.map(item => ([`${item.type.toLowerCase()}${item.name.firstCharInUpperCase()}`, BigInt(item.id)])));
+
+	toItemsArray() {
+		return InventoryItems.filter(item => this.has(BigInt(item.id)));
+	}
+}
+
+export { UserInventory };
--- src/structures/Database/User/index.ts
+++ src/structures/Database/User/index.ts
@@ -1,1 +1,2 @@
 export * from './UserFeatures';
+export * from './UserInventory';
