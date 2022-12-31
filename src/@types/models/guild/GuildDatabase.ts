3fd24de46b20ec87ab997f222f4660585202b9db
jvopinho <jvopinho.dev@gmail.com>
2022-12-31 20:07:34 -0300

    Updating GuildDatabase model

--- src/@types/models/guild/GuildDatabase.ts
+++ src/@types/models/guild/GuildDatabase.ts
@@ -1,6 +1,11 @@
+import { GuildPremiumType } from '@prisma/client';
 import { ObjectType, ID, Field, UseMiddleware } from 'type-graphql';
 
 import DefaultValue from '@utils/DefaultValue';
+
+import { Embed } from '../embed';
+import { GuildPermissions } from './GuildPermissions';
+import { Reason } from './Reason';
 
 const nullable = { nullable: true };
 
@@ -16,8 +21,26 @@ class GuildDatabase {
     	punishments_channel: string;
 
     @Field(type => [String])
-    @UseMiddleware(DefaultValue([]))
+    @UseMiddleware(DefaultValue<string[]>([]))
     	features: string[];
+
+    @Field(type => Embed)
+    @UseMiddleware(DefaultValue<Embed[]>([]))
+    	embeds: Embed[];
+
+    @Field(type => GuildPermissions)
+    @UseMiddleware(DefaultValue<GuildPermissions[]>([]))
+    	permissions: GuildPermissions[];
+
+    @Field(type => Reason)
+    @UseMiddleware(DefaultValue<Reason[]>([]))
+    	reasons: Reason[];
+
+    @Field(type => String, nullable)
+    	premium_type: GuildPremiumType | null;
+
+    @Field(type => Date, nullable)
+    	premium_until: Date | null;
 }
 
 export { GuildDatabase };
