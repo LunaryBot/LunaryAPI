c2188599407bde73b29163602248c2b0e589b570
jvopinho <jvopinho.dev@gmail.com>
2023-02-16 22:42:14 -0300

    Adding GuildController._update with fixing erros

--- src/controllers/GuildController.ts
+++ src/controllers/GuildController.ts
@@ -6,6 +6,7 @@ import { GuildEmbedValidation, GuildGeneralSettingsValidation, GuildPermissionsV
 
 import ApiError from '@utils/ApiError';
 
+import { GuildDatabaseInput } from '@inputs';
 import { GuildDatabase } from '@models';
 import { Embed } from '@types';
 
@@ -21,9 +22,9 @@ interface FullGuildDatabase extends Guild {
 	reasons?: Reason[],
 }
 
-const guildDatabaseSpecialKeys = ['embeds', 'permissions', 'reaons'];
+const guildDatabaseSpecialKeys = ['embeds', 'permissions', 'reasons'];
 
-const embedTypes = Object.keys(EmbedType);
+const acceptEmbedTypes = Object.keys(EmbedType);
 
 class GuildController {
 	public readonly apollo: Apollo;
@@ -99,6 +100,219 @@ class GuildController {
 				guild_id: guildId,
 			},
 		});
+	}
+
+	async _update(guildId: string, raw: GuildDatabaseInput, select: GuildSelect | undefined = undefined, options: { replacePermissions?: boolean } = {}) {
+		let returnData = {} as FullGuildDatabase;
+		
+		if(Object.keys(raw).find(key => !guildDatabaseSpecialKeys.includes(key))) {
+			const currentData = await this.apollo.prisma.guild.findUnique({
+				where: {
+					id: guildId,
+				},
+			}) || {} as Guild;
+
+			const data = GuildGeneralSettingsValidation(raw as any, currentData);
+
+			const d = await this.apollo.prisma.guild.upsert({
+				where: {
+					id: guildId,
+				},
+				create: { ...data, id: guildId },
+				update: data,
+				select: Object.fromEntries(Object.entries(select || {}).filter(([key]) => !guildDatabaseSpecialKeys.includes(key)).map(([key, value]) => ([key, value]))),
+			});
+
+			returnData = { ...returnData, ...d };
+
+			select = Object.fromEntries(Object.entries(select || {}).filter(([key]) => guildDatabaseSpecialKeys.includes(key)).map(([key, value]) => ([key, value])));
+		}
+
+		if(raw.permissions) {
+			const data = GuildPermissionsValidation(raw.permissions);
+
+			const ids = data.map(({ id }) => id);
+
+			const currentData = await this.apollo.prisma.guildPermissions.findMany({
+				where: {
+					guild_id: guildId,
+				},
+			});
+
+			const deleteIds = currentData.filter(({ id }) => !ids.includes(id)).map(({ id }) => id);
+
+			const args: PrismaPromise<any>[] = [];
+
+			if(deleteIds.length && options.replacePermissions === false) {
+				args.push(this.apollo.prisma.guildPermissions.deleteMany({
+					where: {
+						id: {
+							in: deleteIds,
+						},
+						guild_id: guildId,
+					},
+				}));
+			}
+
+			const connectOrCreate = {
+				where: {
+					id: guildId,
+				},
+				create: {
+					id: guildId,
+				},
+			};
+
+			data.forEach(({ id, type, permissions }) => args.push(this.apollo.prisma.guildPermissions.upsert({
+				where: {
+					guild_id_id: {
+						guild_id: guildId,
+						id,
+					},
+				},
+				create: {
+					type, 
+					permissions, 
+					id,
+					Guild: { connectOrCreate },
+				},
+				update: { 
+					permissions,
+					Guild: { connectOrCreate }, 
+				},
+			})));
+
+			const d = await Promise.all(args);
+
+			returnData = { ...returnData, permissions: JSON.parse(JSON.stringify(d, (k, v) => typeof v == 'bigint' ? Number(v) : v)) };
+
+			delete select?.permissions;
+		}
+
+		if(raw.embeds) {
+			const embedTypes = await this.apollo.prisma.embed.findMany({
+				where: {
+					guild_id: guildId,
+				},
+				select: {
+					type: true,
+				},
+			}).then(embeds => embeds.map(embed => embed.type));
+
+			const deleteEmbeds: EmbedType[] = embedTypes.filter(type => !raw.embeds.find(({ type: _type }) => type === _type));
+
+			const args: PrismaPromise<any>[] = [];
+
+			raw.embeds.forEach(_embed => {
+				const { type, ...embed } = _embed as Omit<DatabaseEmbed, 'guild_id'>;
+
+				if(!acceptEmbedTypes.includes(type)) {
+					return;
+
+					// throw new ApiError('Invalid embed type');
+				}
+
+				if(Object.keys(embed).length <= 0 && embedTypes.includes(type)) {
+					return deleteEmbeds.push(type);
+				}
+
+				const data = GuildEmbedValidation(embed);
+
+				args.push(this.apollo.prisma.embed.upsert({
+					where: {
+						guild_id_type: {
+							guild_id: guildId,
+							type,
+						},
+					},
+					create: {
+						guild_id: guildId,
+						type,
+						...data,
+					},
+					update: data,
+				}));
+			});
+
+			if(deleteEmbeds.length) {
+				args.push(this.apollo.prisma.embed.deleteMany({
+					where: {
+						type: {
+							in: deleteEmbeds,
+						},
+						guild_id: guildId,
+					},
+				}));
+			}
+
+			const response = await this.apollo.prisma.$transaction(args);
+
+			returnData = { ...returnData, embeds: response.filter(arg => typeof arg.count == 'undefined') };
+
+			delete select?.embeds;
+		}
+		
+		if(raw.reasons) {
+			const data = GuildReasonsValidation(raw.reasons);
+
+			const ids = data.map(({ id }) => id);
+
+			const currentData = await this.apollo.prisma.reason.findMany({
+				where: {
+					guild_id: guildId,
+				},
+				select: {
+					id: true,
+					text: true,
+				},
+			});
+			
+			const reasonsIds = currentData.map(reason => reason.id);
+				
+			const deleteReasons = currentData.filter(({ id }) => !ids.includes(id));
+
+			const args: PrismaPromise<any>[] = [];
+
+			if(deleteReasons.length) {
+				args.push(this.apollo.prisma.reason.deleteMany({
+					where: {
+						id: {
+							in: deleteReasons.map(({ id }) => id),
+						},
+						guild_id: guildId,
+					},
+				}));
+
+				deleteReasons.forEach(reason => {
+					args.push(this.apollo.controllers.punishments.updateManyReasons(reason.id, reason.text));
+				});
+			}
+
+			data.forEach(reason => {
+				if(reason.id && !reasonsIds.includes(reason.id)) {
+					delete reason.id;
+				}
+
+				args.push(this.apollo.prisma.reason.upsert({
+					where: {
+						guild_id_id: {
+							guild_id: guildId,
+							id: reason.id ?? -1,
+						},
+					},
+					create: { ...reason, guild_id: guildId },
+					update: reason,
+				}));
+			});
+
+			const response = await this.apollo.prisma.$transaction(args);
+
+			returnData = { ...returnData, reasons: response.filter(arg => typeof arg.count == 'undefined') };
+
+			delete select?.reasons;
+		}
+
+		return this.fetchDatabase(guildId, select, returnData);
 	}
 
 	async update(guildId: string, { op, raw }: { op: 'moderation' | 'permissions' | 'embeds' | 'reasons', raw: any }, select: GuildSelect | undefined, { replacePermissions }: { replacePermissions?: boolean } = {}) {
@@ -188,7 +402,7 @@ class GuildController {
 			case 'embeds': {
 				const { type, ...embed } = raw as Omit<DatabaseEmbed, 'guild_id'>;
 
-				if(!embedTypes.includes(type)) {
+				if(!acceptEmbedTypes.includes(type)) {
 					throw new ApiError('Invalid embed type');
 				}
 
